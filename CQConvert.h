5d9982a76a2dbf152bbcdeeec1c209629b6c6a1f
Andrew King <andrew.king@quickplay.com>
2016-06-07 20:14:03 -0400

    Initial Commit. Adds a Conversion utility from the Cryptoquips project

---
+++ CQConvert.h
@@ -0,0 +1,43 @@
+//
+//  CQConvert.h
+//  Cryptoquips
+//
+//  Created by Andrew King on 2015-02-21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CQConvert : NSObject
+
+/**
+ * Returns the index of a string (ie "A" -> 0, "Z" -> 25)
+ */
++ (NSInteger)keyboardAlphaIndexFromString:(NSString *)alphaString;
+
+/**
+ * Returns the index of a character (ie 67 -> 2)
+ */
++ (NSInteger)keyboardAlphaIndexFromCharacter:(unichar)character;
+
+/**
+ * Returns the string representation of an alpha index (ie 0 -> "A", 25 -> "Z")
+ */
++ (NSString *)keyboardAlphaStateStringFromIndex:(NSInteger)index;
+
+/**
+ * Converts a string into an array where each entry represents a letter in the string (ie "ABCZ" -> @[0,1,2,25])
+ */
++ (NSMutableArray *)convertStringToASCIIMutableArray:(NSString *)stringIn;
+
+/**
+ * Converts an array into a string (ie @[0,1,2,25] -> @"ABCZ")
+ */
++ (NSString *)convertASCIIArrayToString:(NSArray *)arrayIn;
+
+/**
+ * Determines if the given integer is the ascii value of a capital letter.
+ */
++ (BOOL)intIsACapitalLetter:(int)intIn;
+
+@end
---
+++ CQConvert.m
@@ -0,0 +1,59 @@
+//
+//  CQConvert.m
+//  Cryptoquips
+//
+//  Created by Andrew King on 2015-02-21.
+//
+//
+
+#import "CQConvert.h"
+
+@implementation CQConvert
+
++ (NSInteger)keyboardAlphaIndexFromString:(NSString *)alphaString {
+    if (alphaString.length > 1 || alphaString.length == 0 || [alphaString isEqualToString:@" "] || [alphaString isEqualToString:@""]) {
+        return -1;
+    }
+    
+    unichar thisChar = [alphaString characterAtIndex:0];
+    return thisChar - 65;
+}
+
++ (NSInteger)keyboardAlphaIndexFromCharacter:(unichar)character {
+    return character - 65;
+}
+
++ (NSString *)keyboardAlphaStateStringFromIndex:(NSInteger)index {
+    if (index < 0 || index > 25) {
+        return nil;
+    }
+    
+    unichar alpha = index + 65;
+    return [NSString stringWithCharacters:&alpha length:1];
+}
+
++ (NSMutableArray *)convertStringToASCIIMutableArray:(NSString *)stringIn {
+    NSMutableArray *arrayOut = [NSMutableArray arrayWithCapacity:stringIn.length];
+    for (int i=0; i<stringIn.length; i++) {
+        unichar ch = [stringIn characterAtIndex:i];
+        if (ch > 96 && ch<123) ch = ch - 32; // convert to all capitals
+        [arrayOut insertObject:[NSNumber numberWithChar:ch] atIndex:i];
+    }
+    return arrayOut ;
+}
+
++ (NSString *)convertASCIIArrayToString:(NSArray *)arrayIn {
+    NSString *stringOut = @"";
+    for (int i=0; i<[arrayIn count]; i++) {
+        //unichar thisChar = (unichar)[arrayIn objectAtIndex:i];
+        NSString *thisChar = [NSString stringWithFormat:@"%c",[[arrayIn objectAtIndex:i] intValue]];
+        stringOut = [stringOut stringByAppendingString:thisChar];
+    }
+    return stringOut;
+}
+
++ (BOOL)intIsACapitalLetter:(int)intIn {
+    return (intIn > 64 && intIn <91);
+}
+
+@end
